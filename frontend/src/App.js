7b5dcba067570506b7cecf42bb5f5afec3b6672a
Willian Parente <willianparente@hotmail.com>
2020-04-03 18:50:43 -0300

    added backend and frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+// useState é utiliza para renderizar na tela a alteração dos estados das variáveis
+import React from 'react';
+import './global.css';
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
